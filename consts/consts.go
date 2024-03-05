b826da5f6be638f8cd7be2747746cdea73323d14
varunmattur111 <154790416+varunmattur111@users.noreply.github.com>
2024-03-05 22:18:59 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "hypersdk"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "VARUN"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "VM"
 )
 
 var ID ids.ID
